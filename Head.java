9018cd5921445edc5bc914f8de6fe4824b378c4e
Abo-Omar-74 <mohamedaboomar1211@gmail.com>
2025-02-08 08:40:49 +0200

    fix(head): resolve logical issue in Head class

--- Head.java
+++ Head.java
@@ -2,20 +2,21 @@ package gitlet;
 
 import java.io.File;
 
-import static gitlet.Utils.writeContents;
+import static gitlet.Utils.*;
 
 public class Head {
-    private final File HEAD_DIR;
+    private final File HEAD_FILE;
 
-    public Head(File headDir) {
-        this.HEAD_DIR = headDir;
+    public Head(File headFile) {
+        this.HEAD_FILE = headFile;
     }
 
-    public File getHeadDir() {
-        return HEAD_DIR;
+    public String getHead() {
+        return readContentsAsString(HEAD_FILE);
     }
 
     public void setHead(Branch branch) {
-        writeContents(HEAD_DIR,branch.getName());
+        writeContents(HEAD_FILE,branch.getName());
     }
+
 }
